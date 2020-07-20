4c78458d39467428c6cba064006229ab425bf022
Jamie Schrifrin <jamieschifrin@hotmail.com>
2020-07-20 08:53:11 -0400

    first

---
+++ app.js
@@ -0,0 +1,34 @@
+//falsy
+
+function bouncer(arr) {
+    let newArray = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i]) newArray.push(arr[i]);
+    }
+    return newArray;
+  }
+
+  //chunky
+
+  function chunkArrayInGroups(arr, size) {
+    var temp = [];
+    var result = [];
+  
+    for (var a = 0; a < arr.length; a++) {
+      if (a % size !== size - 1) temp.push(arr[a]);
+      else {
+        temp.push(arr[a]);
+        result.push(temp);
+        temp = [];
+      }
+    }
+  
+    if (temp.length !== 0) result.push(temp);
+    return result;
+  }
+
+  let dog = {
+
+};
+
+for(var i = 10; i => 0; i = i -2){console.log(i);} 
